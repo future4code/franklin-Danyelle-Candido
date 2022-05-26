891fc88b8578a9faee30247d678ec2188398f592
Danyelleac <danyelleacandido@gmail.com>
2022-05-26 05:57:15 -0700

    [DELETE]COMMENTS

--- modulo1/jsx-labetube/src/App.js
+++ modulo1/jsx-labetube/src/App.js
@@ -21,13 +21,13 @@ function App() {
             </nav>
 
             <section className="painel-de-videos">
-                <CardVideoComponent local={'box-pagina-principal media1'} Url={'https://picsum.photos/400/400?a=1'} VideoTitle={'Terror'}/>//aqui estou usando o componente cardvideo e passando 3 parametros para ele 
+                <CardVideoComponent local={'box-pagina-principal media1'} Url={'https://picsum.photos/400/400?a=1'} VideoTitle={'Terror'}/> 
                 <CardVideoComponent local={'box-pagina-principal media2'} Url={'https://picsum.photos/400/400?a=2'} VideoTitle={'Animação'}/>
-                <CardVideoComponent local={'box-pagina-principal media3'} Url={'https://picsum.photos/400/400?a=3'} VideoTitle={'Amor'}/>//aqui estou usando o componente cardvideo e passando 3 parametros para ele 
+                <CardVideoComponent local={'box-pagina-principal media3'} Url={'https://picsum.photos/400/400?a=3'} VideoTitle={'Amor'}/>
                 <CardVideoComponent local={'box-pagina-principal media4'} Url={'https://picsum.photos/400/400?a=4'} VideoTitle={'Comédia'}/>
-                <CardVideoComponent local={'box-pagina-principal media5'} Url={'https://picsum.photos/400/400?a=5'} VideoTitle={'Vida'}/>//aqui estou usando o componente cardvideo e passando 3 parametros para ele 
+                <CardVideoComponent local={'box-pagina-principal media5'} Url={'https://picsum.photos/400/400?a=5'} VideoTitle={'Vida'}/> 
                 <CardVideoComponent local={'box-pagina-principal media6'} Url={'https://picsum.photos/400/400?a=6'} VideoTitle={'Diversos'}/>
-                <CardVideoComponent local={'box-pagina-principal media7'} Url={'https://picsum.photos/400/400?a=7'} VideoTitle={'Geek'}/>//aqui estou usando o componente cardvideo e passando 3 parametros para ele 
+                <CardVideoComponent local={'box-pagina-principal media7'} Url={'https://picsum.photos/400/400?a=7'} VideoTitle={'Geek'}/>
                 <CardVideoComponent local={'box-pagina-principal media8'} Url={'https://picsum.photos/400/400?a=8'} VideoTitle={'Documentário'}/>              
            </section>
         </main>
