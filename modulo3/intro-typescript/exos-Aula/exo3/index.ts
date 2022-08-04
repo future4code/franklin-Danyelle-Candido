55feb592aa5db84e4d26247de775882d01dd30fa
danyelleac <danyelleacandido@gmail.com>
2022-08-04 13:12:40 -0300

    initial commit

---
+++ modulo3/intro-typescript/exos-Aula/exo3/index.ts
@@ -0,0 +1,10 @@
+function buscarCarrosPorMarca(frota,marca){
+    if(marca === undefined){
+        return frota
+    }
+    return frota.filter(
+        (carro) => {
+            return carro.marca === marca
+        }
+    )
+}
