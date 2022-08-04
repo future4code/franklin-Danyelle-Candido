e48b070f699c423815b9ee04b18f3e5258899da9
danyelleac <danyelleacandido@gmail.com>
2022-08-04 18:02:09 -0300

    subindo exos 1 ao 6

--- modulo3/intro-typescript/exo6/src/index.ts
+++ modulo3/intro-typescript/exo6/src/index.ts
@@ -1,1 +1,21 @@
-//escreva o seu código aqui
+function operacoesMatematicas(a:number,b:number):Array<object> {
+    let soma:number=a+b
+    let subt:number=a-b
+    let mult:number=a*b
+    let maiorNum:number=0
+
+    if(soma>maiorNum){
+        maiorNum=soma
+    }
+    if(subt>maiorNum){
+        maiorNum=subt
+    }
+    if(mult>maiorNum){
+        maiorNum=mult
+    }
+
+    return [{soma},{subt},{mult},{maiorNum}]
+
+}//escreva o seu código aqui
+
+console.log(operacoesMatematicas(4,3))
