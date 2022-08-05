81bc23ba15be8e7035ec1d9666f20cda53a68f83
danyelleac <danyelleacandido@gmail.com>
2022-08-05 10:39:45 -0300

    always

--- modulo3/intro-typescript/exo8/src/index.ts
+++ modulo3/intro-typescript/exo8/src/index.ts
@@ -1,1 +1,13 @@
-//escreva o seu código aqui
+function reverseString(str:string):string {
+    // separo a string em um array["H","e","l","l","o"]
+    let separarString:string[] = str.split(""); // var separarSring = "hello".split("");
+
+    // reverto este array ["o", "l", "l", "e", "h"]
+    let reverterArray:string[] = separarString.reverse(); 
+ 
+    // juntar o array transformando em string "olleh"
+    let juntarArray:string = reverterArray.join("");
+    return juntarArray;
+}
+ 
+console.log(reverseString("hello"))
