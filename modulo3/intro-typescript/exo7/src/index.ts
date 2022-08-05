1b53578c17f3c92190f71210a295bdb323fc65cb
danyelleac <danyelleacandido@gmail.com>
2022-08-05 10:38:49 -0300

    todos exos

--- modulo3/intro-typescript/exo7/src/index.ts
+++ modulo3/intro-typescript/exo7/src/index.ts
@@ -1,1 +1,10 @@
-//escreva o seu código aqui
+function conversorDNA(dna:string):string {
+    // separo a string em um array["H","e","l","l","o"]
+    let converter:string =  dna.replace(/A/g, "U").replace(/T/g, "A").replace(/G/g, "C").replace(/C/g, "G") 
+
+    
+
+    return converter;
+}
+ 
+console.log(conversorDNA("ATTGCTGCGCATTAACGACGCGTA"))
