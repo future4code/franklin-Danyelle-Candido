144729312700213487e611eb949e4d041b04de5b
danyelleac <danyelleacandido@gmail.com>
2022-08-05 11:01:34 -0300

    exo 7

--- modulo3/intro-typescript/exo7/src/index.ts
+++ modulo3/intro-typescript/exo7/src/index.ts
@@ -1,10 +1,19 @@
-function conversorDNA(dna:string):string {
+function conversorDNA(dna:string):string[] {
     // separo a string em um array["H","e","l","l","o"]
-    let converter:string =  dna.replace(/A/g, "U").replace(/T/g, "A").replace(/G/g, "C").replace(/C/g, "G") 
+    let converter:string =  dna.replace(/A/g, "U").replace(/T/g, "A").replace(/G/g, "C");
 
-    
+    let arrayDna:string[] = dna.split(""); 
+    let arrayconverter:string[] = converter.split(""); 
 
-    return converter;
+    for (let i = 0; i < arrayconverter.length; i++) {
+        if(arrayconverter[i]===arrayDna[i]){
+            arrayconverter[i] = arrayconverter[i].replace(/C/g, "G")
+        }
+        
+    }
+    console.log(arrayDna)
+
+    return arrayconverter;
 }
  
 console.log(conversorDNA("ATTGCTGCGCATTAACGACGCGTA"))
