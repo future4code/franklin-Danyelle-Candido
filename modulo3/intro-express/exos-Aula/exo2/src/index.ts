975112cc156fc8393a2f191c06d27a9410b7bb58
danyelleac <danyelleacandido@gmail.com>
2022-08-09 11:30:54 -0300

    exo2-não corrigido

--- modulo3/intro-express/exos-Aula/exo2/src/index.ts
+++ modulo3/intro-express/exos-Aula/exo2/src/index.ts
@@ -11,90 +11,116 @@ app.use(cors())
 
 //Get All Playlists
 
-app.post('/playlists',(request:Request,response:Response)=>{
+app.get('/playlists',(request:Request,response:Response)=>{
 
     const token = request.headers.authorization
     
-    response.status(200).send({
-            "result": {
-                "quantity": "number",
-                "list": [
-                    {
-                        "id": "string",
-                        "name": "string"
-                    }
-                ]
-            }
-        })
-
+    if(!token){
+        response.status(401).send({message:'Unauthorized'})
         
-    })
+    }
+    response.status(200).send({
+        "result": {
+            "quantity": "number",
+            "list": [
+                {
+                    "id": "string",
+                    "name": "string"
+                }
+            ]
+        }
+    })   
+})
+
 
 //Get Playlist Tracks
 
+app.get('/playlists/:playlistId/tracks',(request:Request,response:Response)=>{
+    
+    const token = request.headers.authorization
+    
+    const playlistTrackId = Number(request.params.id)
+    const trackId = [{id:1,playlist:'Samba'},{id:2,playlist:'Reggae'}]
+    
+    const acharTrack= trackId.find((track)=>{
+        return track.id === playlistTrackId
+    })
+    if(!token){
+        response.status(401).send({message:'Unauthorized'})
+        
+    }else if(!acharTrack){
+        response.status(401).send({message:'Unauthorized'})
+    }
+    response.status(200).send({
+        "result": {
+            "quantity": "number",
+            "tracks": [ 
+                {
+                    "id": "string", 
+                    "name": "string", 
+                    "artist": "string",
+                    "url": "string"
+                }
+            ]
+        }
+    })
 
+    
+})
 //Delete Playlist
 
-//Delete Track
-
-app.get('/nome',(request:Request,response:Response)=>{
-    //retorna uma resposta ao user com o status 200 e envia oq esta no send ()
-    const retorno='primeiro endpoint criado'
-    response.status(202).send({message:retorno})
-})
-
-app.post('/token',(request:Request,response:Response)=>{
-/*     o req header serve para quando queremos solicitar ao nosso user algo no headers como por exemplo a authorization */
+app.delete('/playlists/:playlistId',(request:Request,response:Response)=>{
+    
     const token = request.headers.authorization
-    console.log(token)
-
-    response.status(200).send({message:"TOKEN ENVIADO"})
-})
-
-app.post('/enviar-nome',(request:Request,response:Response)=>{
-    /* o req body é para quando precisamos passar algo com mais de uma linha , por exemplo um objeto um json, um yaml */
-        const cadastrar = request.body
-        console.log(cadastrar)
     
-        //aqui eu retorno uma resposta
-        response.status(201).send({message:"cadastrado"})
-})
-
-app.post('/encontrar-prod',(request:Request,response:Response)=>{
-/* o req query é quando preciso fazer filtros ou páginação*/
-const array=['teclado', 'mouse', 'monitor']
-        const {produto} = request.query
-
-        const [achar]=array.filter((prod)=>{
-            return prod === produto
-        })
-
-        if(!achar){
-            //throw new Error('não achei o produto')- se não achar posso usar isso que da um erro bem feio ou uma response conforme abaixo
-            response.status(404).send({message:'produto not found'})
-        }
-        console.log(produto)
-
-        response.status(201).send(`produto ${achar} encontrado`)
-        })
-
-app.post('/nome/:id',(request:Request,response:Response)=>{
-    /* o req params é para  passar parametros como por exemplo o id */
-    const id = Number(request.params.id)
-    const arrayId = [{id:1,nome:'Gabriel'},{id:2,nome:'Murilo'}]
-
-    const achar= arrayId.find((person)=>{
-        return person.id === id
+    const playlistId = Number(request.params.id)
+    const arrayId = [{id:1,playlist:'Samba'},{id:2,playlist:'Reggae'}]
+    
+    const acharPlaylist= arrayId.find((playlist)=>{
+        return playlist.id === playlistId
     })
-    
-        //aqui eu retorno uma resposta
-        response.status(200).send({message:`Bem vindx!${achar?.nome}`})
-/*         response.status(201).end() -usamos quando não queremos retornar nada ao user */
-})    
 
+
+    if(!token){
+        response.status(401).send({message:'Unauthorized'})
+        
+    }else if(!acharPlaylist){
+        response.status(401).send({message:'Unauthorized'})
+    }
+    response.status(200).send({message:'Deleted Playlist'})
+
+})
+
+//Delete Track
+app.delete('playlists/:playlistId/tracks/:trackId',(request:Request,response:Response)=>{
+    
+    const token = request.headers.authorization
+    
+    const playlistId = Number(request.params.id)
+    const arrayId = [{id:1,playlist:'Samba'},{id:2,playlist:'Reggae'}]
+    
+    const acharPlaylist= arrayId.find((playlist)=>{
+        return playlist.id === playlistId
+    })
+
+    const playlistTrackId = Number(request.params.id)
+    const trackId = [{id:1,musica:'Clayton Rasta'},{id:2,musica:'Cabeça de Gelo'}]
+    
+    const acharTrack= trackId.find((track)=>{
+        return track.id === playlistTrackId
+    })
+
+    if(!token){
+        response.status(401).send({message:'Unauthorized'})
+        
+    }else if(!acharPlaylist || !acharTrack){
+        response.status(401).send({message:'Unauthorized'})
+    }
+    response.status(200).send({message:'Deleted track'})
+
+})
 
 app.listen(3003,()=>{
     console.log("Server is running in http://localhost:3003")
 })//aqui  eu falo que meu app esta escutando (rodando) na porta 3003 e faço uma arrow function callback mostrando onde ele esta rodando
 
-console.log("Hello, world!")
