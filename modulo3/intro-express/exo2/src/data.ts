ebd54ca2990529fadec99ae05a87656f6951e806
danyelleac <danyelleacandido@gmail.com>
2022-08-11 15:44:42 -0300

    exos 1 ao 8

---
+++ modulo3/intro-express/exo2/src/data.ts
@@ -0,0 +1,31 @@
+
+  export const users = [{
+    "id": 1,
+    "name": "Leanne Graham",
+    "email": "Sincere@april.biz",
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org"
+  },
+  {
+    "id": 8,
+    "name": "Nicholas Runolfsdottir V",
+    "email": "Sherwood@rosamond.me",
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com"
+
+  },
+  {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "email": "Chaim_McDermott@dana.io",
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com"
+  },
+  {
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "email": "Rey.Padberg@karina.biz",
+    "phone": "024-648-3804",
+    "website": "ambrose.net"
+  }
+]
