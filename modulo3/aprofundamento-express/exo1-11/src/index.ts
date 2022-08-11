bf2bea6a0b229f3a84c1a089a2eb0bde62c18e24
danyelleac <danyelleacandido@gmail.com>
2022-08-11 20:20:48 -0300

    initial commit

---
+++ modulo3/aprofundamento-express/exo1-11/src/index.ts
@@ -0,0 +1,91 @@
+import express, { Request, Response} from 'express'
+import cors from 'cors'
+import { userAndPosts } from "./data"//achei melhor colocar junto porque fica amarrado , da outra forma acho que pode haver falhas por exemplo um usuario ser excluido e por algum motivo os posts permanecerem 
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+type posts= {
+        id: number,
+        title: string,
+        body: string
+      }
+
+type user= {
+    id: number,
+    name:string,
+    email:string,
+    phone:string,
+    website: string,
+    postagens: posts[]
+}
+
+
+app.listen(3003,()=>{
+    console.log("Server is running in http://localhost:3003")
+})
+
+//postagens e usuarios
+app.get('/feedpage',(request:Request,response:Response)=>{
+    const feed:user[] =userAndPosts
+    response.status(202).send(feed)
+
+})
+
+//postagens por usuario
+app.get('/feedpage/:id',(request:Request,response:Response)=>{
+    const idPerson = Number(request.params.id)
+
+    const findPerson = userAndPosts.filter((user)=>{
+        return user.id === idPerson
+    })
+
+    response.status(202).send(findPerson)
+
+})
+
+//criando postagem
+app.post("/createPost/:id",(request:Request,response:Response)=>{
+    const idPerson = Number(request.params.id)
+
+    const {id,title,body} = request.body
+
+    const findPerson = userAndPosts.find((user)=>{
+        return user.id === idPerson
+    })//procuro a pessoa
+
+   findPerson?.postagens.push({id,title,body})
+   response.status(201).send(findPerson)
+
+  })
+
+  //deletando postagem
+
+  app.delete("/posts/:id", (request: Request, response: Response) => {
+    const idPerson = Number(request.params.id)
+    const postId = Number(request.query.postId);
+
+    const findPerson = userAndPosts.find((user)=>{
+        return user.id === idPerson
+    })//procuro a pessoa
+    
+    const deletarDado= findPerson?.postagens.filter((post) => {
+        return post.id !== postId
+    })
+
+    console.log(deletarDado)
+    response.send(deletarDado);
+});
+
+  //deletando usuario
+
+  app.delete("/users/:id", (request: Request, response: Response) => {
+    const idPerson = Number(request.params.id)
+    
+    const deletarPerson= userAndPosts?.filter((user) => {
+        return user.id !== idPerson
+    })
+
+    response.send(deletarPerson);
+});
