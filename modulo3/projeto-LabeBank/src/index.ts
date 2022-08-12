4e5ae02b7f2218549a17cbb3dfed5a173903b6e6
danyelleac <danyelleacandido@gmail.com>
2022-08-12 18:13:16 -0300

    exo 7

--- modulo3/projeto-LabeBank/src/index.ts
+++ modulo3/projeto-LabeBank/src/index.ts
@@ -25,16 +25,47 @@ app.post('/createAccount',(req:Request,res:Response)=>{
     const listarCorrentistas = correntistas
     const {cpf,name,dataNascimento} = req.body
 
-    const novaConta : correntista ={
-        cpf,
-        name,
-        dataNascimento,
-        saldo:0,
-        extrato:[],
+    function maiorIdade(dataNascimento:string):boolean{
+        
+        const d = new Date()//peguei a data atual completa mas ela vem com horas e fora do formato que eu espero
+        const diaAtual = d.getDate()
+        const mesAtual = d.getMonth()+1//acrescento mais um pq ele conta de 0 a 11
+        const anoAtual = d.getFullYear()
+        
+        const n = dataNascimento.split("/")//separando por barra
+        const diaNascimento = Number(n[0])
+        const mesNascimento = Number (n[1])
+        const anoNascimento = Number(n[2])
+
+
+        if((anoAtual-anoNascimento)>18){
+            return true
+        }else if((anoAtual-anoNascimento)===18 && mesAtual>mesNascimento) {
+            return true
+        }else if((anoAtual-anoNascimento)===18 && mesAtual===mesNascimento && diaAtual>=diaNascimento ) {
+            return true
+        }else{
+            return false
+        }
+        
     }
 
-    listarCorrentistas?.push(novaConta)
+    if(maiorIdade(dataNascimento)){
 
-    res.status(201).send(listarCorrentistas)
+        const novaConta : correntista ={
+            cpf,
+            name,
+            dataNascimento,
+            saldo:0,
+            extrato:[],
+        }
+
+        listarCorrentistas?.push(novaConta)
+
+        res.status(201).send(listarCorrentistas)
+
+    }else{
+        res.status(400).send({message:"Não autorizada a abertura de conta para menores de 18 anos"})
+    }
 
 })
