4913ef15efad4db9eb91c21fe2125c2ed2ca1961
danyelleac <danyelleacandido@gmail.com>
2022-08-25 21:54:25 -0300

    desafio 6

--- modulo3/projeto-to-do-list/src/endpoints/getTaskByUserId.ts
+++ modulo3/projeto-to-do-list/src/endpoints/getTaskByUserId.ts
@@ -8,26 +8,8 @@ export default async function getTaskByUserId(req:Request, res:Response)  {
             res.status(404).send("not found")
         }         
         res.status(200).send({
-            "tasks":[
-                {
-                    "taskId": result.id
-                }
-            ]
-        }
-            
-            
-/*             result
-
-
-            id: result.id,
-            title:result.title,
-            description:result.description,
-            limitDate: moment(result.limit_date,'YYYY-MM-DD').format('DD/MM/YYYY'), 
-            status: result.status,
-            creatorUserId:result.creator_user_id,
-            nickname: result.nickname */
-        
-        )
+            "tasks": result
+        })
 
     }catch(error){
         res.status(400).send(error)
