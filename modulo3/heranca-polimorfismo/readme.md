9fa1d7bfcb1bf56336793ee3eb1b5654cdf1ecf4
danyelleac <danyelleacandido@gmail.com>
2022-09-01 10:11:00 -0300

    exos 1 ao 5

---
+++ modulo3/heranca-polimorfismo/readme.md
@@ -0,0 +1,18 @@
+##Exo1
+a)Não porque não há um get em password
+
+b)Uma vez
+
+
+##Exo2
+a)Uma vez
+
+B)Uma vez , porque dentro do Customer temos a extenção se user
+
+##Exo3
+
+a)Não porque não temos um get em password
+
+##Exo4
+
+##Exo5
--- modulo3/heranca-polimorfismo/src/index.ts
+++ modulo3/heranca-polimorfismo/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!")
+import { Customer } from "./class/Customer";
+
+const ACustomer = new Customer("1122jj","maria@yahoo.com","Maria","12gq","1232");
+
+console.log(ACustomer.getName())
+console.log(ACustomer.getId())
+console.log(ACustomer.getEmail())
+console.log(ACustomer.getCreditCard())
+console.log(ACustomer.interoduceYourself())
