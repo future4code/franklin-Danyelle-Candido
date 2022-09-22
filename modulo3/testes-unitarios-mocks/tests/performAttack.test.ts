72e2a9b601f8c0c916d51a45a4f6987cb56617f1
danyelleac <danyelleacandido@gmail.com>
2022-09-22 10:53:01 -0300

    exos 4 e 5

---
+++ modulo3/testes-unitarios-mocks/tests/performAttack.test.ts
@@ -0,0 +1,82 @@
+import {Character,performAttack} from "../src/index"
+
+//exo4 a decidi criar da performAttack porque a da validateCharacter eu já testei
+
+/* exemplo
+test("Showing jest.fn", () => {
+	const mock = jest.fn(() => {
+		const user = {
+				name: "Astrodev",
+				age: 29
+		}
+		return user
+	})
+}) */
+test("Creating Mocks", () => {
+    const validatorMock = jest.fn(() => {
+			return true
+		});
+});
+
+test("Creating Mocks", () => {
+    const validatorMock = jest.fn(() => {
+			return false
+		});
+});
+
+test("Should perform attack", () => {
+    const validatorMock = jest.fn(() => {
+      return true;
+    });
+
+    const attacker: Character = {
+      name: "Scorpion",
+      life: 1500,
+      defense: 200,
+      strength: 600,
+    };
+
+    const defender: Character = {
+      name: "Kitana",
+      life: 1500,
+      defense: 400,
+      strength: 800,
+    };
+
+    performAttack(attacker, defender, validatorMock as any);
+
+    expect(defender.life).toEqual(1300);
+    expect(validatorMock).toHaveBeenCalled();
+    expect(validatorMock).toHaveBeenCalledTimes(2);
+    expect(validatorMock).toHaveReturnedTimes(2);
+  });
+
+  test("Should return invalid character error", () => {
+    expect.assertions(4);
+    const validatorMock = jest.fn(() => {
+      return false;
+    });
+
+    const attacker: Character = {
+      name: "",
+      life: 1500,
+      defense: 200,
+      strength: 600,
+    };
+
+    const defender: Character = {
+      name: "Kitana",
+      life: 1500,
+      defense: 400,
+      strength: 800,
+    };
+
+    try {
+      performAttack(attacker, defender, validatorMock as any);
+    } catch (err:any) {
+      expect(err.message).toEqual("Invalid character");
+      expect(validatorMock).toHaveBeenCalled();
+      expect(validatorMock).toHaveBeenCalledTimes(1);
+      expect(validatorMock).toHaveReturnedTimes(1);
+    }
+  });
